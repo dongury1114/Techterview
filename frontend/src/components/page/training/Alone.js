27c22fdf070b80074eb57953f6019d3be6221d25
tkdgh <ktkdgh821@gmail.com>
2022-07-17 00:29:02 +0900

    questions req-res ing

--- frontend/src/components/page/training/Alone.js
+++ frontend/src/components/page/training/Alone.js
@@ -1,30 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
+import axios from 'axios';
 
 function TrainingAlone() {
     const { key } = useParams();
+    let data = []
+    
+    axios.get(`http://localhost:8000/training/alone/api/questions/${key}`)
+    .then((res) => {
+        console.log(res);
+        (res.data.value).forEach((value) => {
+            data.push(value)
+            // cnt = cnt + 1;
+        });
+    })
+    
+    const [ Questions , SetQuestions ] = useState(data);
+    
+    // let i = 0
+    // SetQuestions = () => {
+    //     Questions = Questions[i];
+    // }
 
-    const getQuestion = async () => {
-        const json = await (
-            fetch(`http://localhost:8000/traing/questions/${key}`)
-        ).json()
-        console.log(json());
 
-    }
     return (
         <div className="training-container">
             <Helmet><style>{'body { background-color: black; }'}</style></Helmet>
             <div className="training-navigation-bar-logo"> TECHTERVIEW </div>
             <div className='traing-inner-box'>
+                <div>{Questions}</div>
                 <div className='training-alone-dropbox'></div>
             </div>
             <div className='training-footer'>
                 <div className="training-container-box">
-                    <div className="training-item1">1<img className="video-thumbnail-second-place" src={require("../../images/start.png")} alt={"start button"} /></div>
+                    <div className="training-item1" ><img className="video-thumbnail-second-place" src={require("../../images/start.png")} alt={"start button"} /></div>
                     <div className="training-item-first-place"><img className="video-thumbnail-first-place" src={require("../../images/next.png")} alt={"next button"} />   </div>
                     <div className="training-item"><img className="video-thumbnail-third-place" src={require("../../images/end.png")} alt={"end button"} />   </div>
                 </div>
